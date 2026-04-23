aa94cc13f6f71b19a218a9c34d6a24bbc5ad13b1
Flyyyy <66690340+Wayy2Flyyy@users.noreply.github.com>
2026-04-23 10:50:02 +0100

    Create admin.lua

---
+++ shared/config/admin.lua
@@ -0,0 +1,16 @@
+local Config = DDHunting.Config
+
+Config.Admin = {
+    Commands = {
+        SpawnAnimal = 'huntspawn',
+        ClearAnimals = 'huntclear',
+        SetHeat = 'huntheat',
+        GiveLicense = 'huntlicense',
+        StartLegendary = 'huntlegendary',
+    },
+
+    Logging = {
+        Enabled = true,
+        PrintToConsole = true,
+    }
+}
