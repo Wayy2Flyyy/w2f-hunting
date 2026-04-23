49d58543467665fa2f868d243872e966ff9f1496
Flyyyy <66690340+Wayy2Flyyy@users.noreply.github.com>
2026-04-23 11:10:45 +0100

    Create init.lua

---
+++ client/init.lua
@@ -0,0 +1,4 @@
+DDHunting.Client = DDHunting.Client or {}
+DDHunting.Client.State = DDHunting.Client.State or {}
+DDHunting.Client.Systems = DDHunting.Client.Systems or {}
+DDHunting.Client.Bridge = DDHunting.Client.Bridge or {}
