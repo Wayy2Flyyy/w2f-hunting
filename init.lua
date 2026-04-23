2938bce2ef7f5315c1d837c25997891423600e07
Flyyyy <66690340+Wayy2Flyyy@users.noreply.github.com>
2026-04-23 10:39:32 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+DDHunting = DDHunting or {}
+DDHunting.Config = DDHunting.Config or {}
+DDHunting.Data = DDHunting.Data or {}
+DDHunting.Utils = DDHunting.Utils or {}
+
+local Config = DDHunting.Config
+
+Config.ResourceName = GetCurrentResourceName()
+Config.Debug = false
+Config.Locale = 'en'
+Config.Version = '0.1.0-foundation'
