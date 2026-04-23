46b6b6696b753bcb424c5f8166ac58c0d8ec8ea7
Flyyyy <66690340+Wayy2Flyyy@users.noreply.github.com>
2026-04-23 11:06:32 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+
+    'server/init.lua',
+    'server/state.lua',
+
+    'server/bridge/esx.lua',
+    'server/bridge/ox_inventory.lua',
+    'server/bridge/oxmysql.lua',
+
+    'server/services/wildlife_service.lua',
+
+    'server/main.lua',
+}
