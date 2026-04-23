279f3a46139027eb2a531250cffcba962dd41ffd
Flyyyy <66690340+Wayy2Flyyy@users.noreply.github.com>
2026-04-23 11:01:51 +0100

    Create init.lua

---
+++ server/init.lua
@@ -0,0 +1,4 @@
+DDHunting.Server = DDHunting.Server or {}
+DDHunting.Server.Bridge = DDHunting.Server.Bridge or {}
+DDHunting.Server.Services = DDHunting.Server.Services or {}
+DDHunting.Server.State = DDHunting.Server.State or {}
